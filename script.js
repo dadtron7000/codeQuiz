cb697356049daf9432334350a3a769207697d0f9
dadtron7000 <kyle.steiner7@gmail.com>
2021-06-29 17:56:00 -0500

    added functions for quiz operation

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 var startBtn = document.getElementById("startBtn");
 var questionPlace = document.getElementById("question");
 var answerPlace = document.getElementById("answers");
+var rightWrongPlace = document.getElementById("rightWrong")
 var timerEl = document.getElementById("timer");
-const questions = [".setAttribute() is a javascript _____."];
+const questions = [
+  ".setAttribute() is a javascript _____.",
+  "There are __ falsey values",
+];
 const question1Answers = ["method", "function", "property", "pizza"];
+const question2Answers = ["2", "9", "6", "1"];
 var timeAmount = 60;
 let countDown;
+let answerItem;
+let rightWrongAlert;
+
+function question1() {
+  questionPlace.textContent = questions[0];
+
+  for (let i = 0; i < question1Answers.length; i++) {
+    let answerItem = document.createElement("li");
+    answerItem.textContent = question1Answers[i];
+    answerItem.setAttribute("class", "list-group-item");
+    console.log(answerItem);
+    answerPlace.appendChild(answerItem);
+  }
+}
+function question2() {
+  questionPlace.textContent = questions[1];
+
+  for (let i = 0; i < question2Answers.length; i++) {
+    let answerItem = document.createElement("li");
+    answerItem.textContent = question2Answers[i];
+    answerItem.setAttribute("class", "list-group-item");
+    console.log(answerItem);
+    answerPlace.appendChild(answerItem);
+  }
+}
 
 function timer(event) {
   event.preventDefault();
@@ -35,9 +65,13 @@ function gameStart(event) {
     quizEl[i].style.display = "block";
   }
 
-  questionPlace.innerHTML = questions[0]
-
+  question1();
 }
 
-startBtn.addEventListener("click", gameStart);
+function answerChoice(event) {
+  event.preventDefault();
+}
+
 startBtn.addEventListener("click", timer);
+startBtn.addEventListener("click", gameStart);
+answerPlace.addEventListener("click", answerChoice);
